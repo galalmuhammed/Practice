e8f3b3820171a1273c5dd7af8496cfef0007a0b0
Galal <galal@gmail.com>
2025-09-14 19:30:36 +0300

    Initiated script file and added basic logic for buttons

---
+++ script.js
@@ -0,0 +1,11 @@
+const head = document.getElementById("header-one");
+
+const changeColor = () => {
+  head.style.color = "brown";
+  head.style.backgroundColor = "beige";
+};
+
+const resetColor = () => {
+  head.style.color = "#333";
+  head.style.backgroundColor = "#fff";
+};
